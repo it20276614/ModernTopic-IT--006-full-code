1af132768b2e3ac1e3a438082ebb0018299cc013
M.A.Miqdad Ali Riza <73341274+it20276614@users.noreply.github.com>
2023-04-19 14:35:06 +0530

    Add files via upload

---
+++ Sliit.MTIT.Student/Data/StudentMockDataService.cs
@@ -0,0 +1,14 @@
+﻿namespace Sliit.MTIT.Student.Data
+{
+    public class StudentMockDataService
+    {
+        public static List<Models.Student> Students = new List<Models.Student>()
+        {
+            new Models.Student {Id = 1, Name = "miqdad", Address = "123 Main sammanthurai-02", Age = 20, Email="miqdad@gmail.com", Phonenumber = 0764517896},
+            new Models.Student {Id = 2, Name = "dasun", Address = "442 kalubowil  colombo", Age = 22, Email="dasun@gmail.com", Phonenumber = 0764723802},
+            new Models.Student {Id = 3, Name = "Sanaka", Address = "456 kandy", Age = 21, Email = "sanaka@gmail.com", Phonenumber = 0765698741},
+            new Models.Student {Id = 4, Name = "wimuthi", Address = "321 bamalapitya St", Age = 23, Email = "wimuthi@gmail.com", Phonenumber = 0764519636},
+            new Models.Student {Id = 5, Name = "deri", Address = "654 dehewala", Age = 19, Email = "deri@gmail.com", Phonenumber = 0764517751},
+        };
+    }
+}
---
+++ Sliit.MTIT.Student/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Sliit.MTIT.Student.Models
+{
+    
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public int Age { get; set; }
+
+        public string? Email { get; set; }
+
+        public int Phonenumber { get; set; }
+
+    }
+}
